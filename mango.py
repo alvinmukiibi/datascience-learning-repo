63bd3fe0701833b3123ac5399569213bfb4a2245
Alvin Mukiibi <mukikelly792@gmail.com>
2020-03-27 09:23:26 +0300

    variables and types

--- mango.py
+++ mango.py
@@ -2,12 +2,33 @@ print "Hello World"
 
 
 import math
-from math import *
-from math import cos, pi
-x = cos(90)
+# from math import *
+# from math import cos, pi
+# x = cos(90)
 
-for md in dir(math):
-    print md
+# for md in dir(math):
+#     print md
+#
+# help(math.ceil) # to describe what a function does
 
-help(math.ceil) # to describe what a function does
+my_age = 23
+#
+# print type(my_age)
+#
+# x = 1.0 - 2.0j
+#
+# print type(x)
+# print x
 
+import types # modules contains a number of type name defns to test if variables are of a certain type
+
+
+r = 3.4
+
+is_it = type(r) is float
+is_it_2 = isinstance(r, str)
+print is_it_2
+
+my_complex = 2 + 3j
+
+print my_complex.imag, my_complex.real
