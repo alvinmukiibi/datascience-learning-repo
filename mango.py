ee5605c9cc6ad7bb9090e0c667df052ac2b6cdd6
Alvin Mukiibi <mukikelly792@gmail.com>
2020-03-27 09:22:25 +0300

    modules and imports

--- mango.py
+++ mango.py
@@ -1,1 +1,13 @@
 print "Hello World"
+
+
+import math
+from math import *
+from math import cos, pi
+x = cos(90)
+
+for md in dir(math):
+    print md
+
+help(math.ceil) # to describe what a function does
+
